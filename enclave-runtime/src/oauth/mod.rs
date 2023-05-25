2e70000dce690bfcc9d097cb9045993bf0ff98d2
Titus Abele <tvmab@pm.me>
2023-05-25 11:22:25 +0200

    made tcp echo server

--- enclave-runtime/src/oauth/mod.rs
+++ enclave-runtime/src/oauth/mod.rs
@@ -1,21 +1,46 @@
 
 use sgx_tstd as std;
-use std::io::Read;
-use std::io::Write;
-use std::vec::{Vec};
-use std::net::{TcpListener, TcpStream};
-use std::str;
+use std::thread;
+use std::str::from_utf8;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream, Shutdown};
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8; 50]; // using 50 byte buffer
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            // echo everything!
+            stream.write(&data[0..size]).unwrap();
+            true
+        },
+        Err(_) => {
+            println!("[SERVER]: An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
 
 pub fn start_oauth_server() {
     println!("Hello World from inside the Enclave!");
 
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    
+    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    // accept connections and process them, spawning a new thread for each one
+    println!("[SERVER]: Server listening on port 7878");
+
     for stream in listener.incoming() {
-        let mut buffer = Vec::new();
-        let mut stream = stream.unwrap();
-        stream.read_to_end(&mut buffer);
-        let message = str::from_utf8(&buffer);
-        println!("Message received: {}", message.unwrap());
+        match stream {
+            Ok(stream) => {
+                println!("[SERVER]: New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    // connection succeeded
+                    handle_client(stream)
+                });
+            }
+            Err(e) => {
+                println!("[SERVER]: Error: {}", e);
+                /* connection failed */
+            }
+        }
     }
 }
