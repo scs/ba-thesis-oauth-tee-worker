0b141fad6b7790dc3af681e408b1c8cfb08dd6a9
Titus Abele <tvmab@pm.me>
2023-05-25 10:38:59 +0200

    made a simple tcp listener on enclave

--- enclave-runtime/src/oauth/mod.rs
+++ enclave-runtime/src/oauth/mod.rs
@@ -1,3 +1,21 @@
+
+use sgx_tstd as std;
+use std::io::Read;
+use std::io::Write;
+use std::vec::{Vec};
+use std::net::{TcpListener, TcpStream};
+use std::str;
+
 pub fn start_oauth_server() {
-    println!("Hello World from inside the Enclave!")
+    println!("Hello World from inside the Enclave!");
+
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    
+    for stream in listener.incoming() {
+        let mut buffer = Vec::new();
+        let mut stream = stream.unwrap();
+        stream.read_to_end(&mut buffer);
+        let message = str::from_utf8(&buffer);
+        println!("Message received: {}", message.unwrap());
+    }
 }
